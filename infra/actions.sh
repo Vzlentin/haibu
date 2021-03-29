79d1b3407c2f31fb80f977e5475cccc1eb62ef5d
Valentin DUSSERRE <valentindusserre@gmail.com>
2021-03-29 11:38:39 +0200

    fix infra

--- infra/actions.sh
+++ infra/actions.sh
@@ -5,14 +5,16 @@ then
 	git clone https://github.com/vzlentin/haibu
 	sudo mv haibu /opt/
 	sudo chown -R pi:pi /opt/haibu
-	cd /opt/haibu/infra
-	python3 createdb.py
 	pip3 install -r /opt/haibu/requirements.txt
-	sudo mv /opt/haibu/infra/haibu.service /etc/systemd/system/
+	sudo cp /opt/haibu/infra/haibu.service /etc/systemd/system/
 else
 	cd /opt/haibu
 	git pull
+	sudo rm app.db
 fi
+
+cd /opt/haibu/infra
+python3 createdb.py
 
 sudo systemctl daemon-reload
 sudo systemctl start haibu.service
