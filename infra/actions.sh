bdc00c67543ed5e93ce19601c9390c34e34e2339
Valentin DUSSERRE <valentindusserre@gmail.com>
2021-04-28 18:22:12 +0200

    update service when ff

--- infra/actions.sh
+++ infra/actions.sh
@@ -5,12 +5,12 @@ then
 	git clone https://github.com/vzlentin/haibu
 	sudo mv haibu /opt/
 	sudo chown -R pi:pi /opt/haibu
-	pip3 install -r /opt/haibu/requirements.txt
-	sudo cp /opt/haibu/infra/haibu.service /etc/systemd/system/
 else
 	cd /opt/haibu
 	git pull
 fi
+
+pip3 install -r /opt/haibu/requirements.txt
 
 MEDIA_PATH=$(grep MEDIA_PATH /opt/haibu/.flaskenv | cut -d= -f2 | sed "s/\"//g")
 
@@ -20,6 +20,7 @@ sudo ln -sn $MEDIA_PATH/Scans /opt/haibu/app/scans/static
 cd /opt/haibu/utils
 python3 syncdb.py
 
+sudo cp /opt/haibu/infra/haibu.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable --now haibu.service
 
